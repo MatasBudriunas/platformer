0b40a1769b8dd8fc0ecc9f6c7daf74050c8dc0bb
MatasBudriunas <zewas1m@gmail.com>
2024-07-21 13:20:27 +0700

    Fix - remove debugging and add styling

--- Enemy.gd
+++ Enemy.gd
@@ -6,13 +6,10 @@ extends Area2D
 var start_pos : Vector2
 var target_pos : Vector2
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	start_pos = global_position
 	target_pos = start_pos + move_dir
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = global_position.move_toward(target_pos, move_speed * delta)
 	
@@ -21,9 +18,6 @@ func _process(delta):
 			target_pos = start_pos + move_dir
 		else:
 			target_pos = start_pos
-	
-
-
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
